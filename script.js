a61fd6fc91dfd1d3c752b88c6b8e6e833b46f37c
Dana Alami <dana.alami18@gmail.com>
2021-03-26 19:42:46 +0300

    added api key and fetched it

---
+++ script.js
@@ -0,0 +1,13 @@
+var API_KEY = 'b09197d74428f8d485f51ba02ad2e29c';
+
+function weatherData (cityName) {
+    fetch('api.openweathermap.org/data/2.5/weather?q=' + cityName +'&appid=' + API_KEY)
+    .then(response => response.json())
+    .then(data => {
+        console.log(data)
+        var currentWeather = data.weather[0].main
+        displayWeather(currentWeather)
+    });
+}
+
+
