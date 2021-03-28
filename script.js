2dbdb647903f60619e9cc79ea2d09281a3a8a9de
Dana Alami <dana.alami18@gmail.com>
2021-03-28 12:36:21 +0300

    added function to display weather and geolocation

--- script.js
+++ script.js
@@ -1,20 +1,56 @@
 var API_KEY = 'b09197d74428f8d485f51ba02ad2e29c';
+var figureEl = document.getElementById('#current-weather');
+console.log(figureEl)
 
 function weatherData (cityName) {
-    fetch('api.openweathermap.org/data/2.5/weather?q=' + cityName +'&appid=' + API_KEY)
+    fetch('https://api.openweathermap.org/data/2.5/weather?q=' + cityName +'&appid=' + API_KEY)
     .then(response => response.json())
-    .then(data => {
-        console.log(data)
-        var currentWeather = data.weather[0].main
-        displayWeather(currentWeather)
-    });
+    .then(data => console.log(data));
+    //     console.log(data)
+    //     var currentWeather = data.weather[0].main
+    //     displayWeather(currentWeather)
+    // });
 }
 
-getWeatherData('Jerusalem')
+ 
+
+ var icons = {
+    clearSky: '01d'
+}
 
 function returnIconURL (code) {
-    return ' http://openweathermap.org/img/wn' + code + '.png'
-}
+    return  'http://openweathermap.org/img/wn/' + code + '.png'
+ }
 
+ 
+ returnIconURL(icons.clearSky)
 
+ function displayWeather(currentWeather){
+     figureEl.innerHTML = ''
+     var currentWeather = icons[currentWeather]
+     var imEl = document.createElement('img')
+     imgEl.setAttribute('src', returnIconURL(currentWeather));
+    figureEl.appendChild(imgEl)
+ }
+ getWeatherData('Jerusalem')
 
+ var options = {
+    enableHighAccuracy: true,
+    timeout: 5000,
+    maximumAge: 0
+  };
+  
+  function success(pos) {
+    var crd = pos.coords;
+  
+    console.log('Your current position is:');
+    console.log(`Latitude : ${crd.latitude}`);
+    console.log(`Longitude: ${crd.longitude}`);
+    console.log(`More or less ${crd.accuracy} meters.`);
+  }
+  
+  function error(err) {
+    console.warn(`ERROR(${err.code}): ${err.message}`);
+  }
+  
+  navigator.geolocation.getCurrentPosition(success, error, options);
