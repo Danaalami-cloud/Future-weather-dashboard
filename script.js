6640c1b9326bafd675083809178e0877c61f44df
Dana Alami <dana.alami18@gmail.com>
2021-04-16 23:07:07 +0300

    changes made in html, fixing code in js and inserting image in css

--- script.js
+++ script.js
@@ -4,8 +4,23 @@ var figureEl = document.getElementById("#current-weather");
 var searchEl = document.getElementById("search-button");
 var clearEl = document.getElementById("clear-button");
 
-function getWeatherData() {
-    var cityName = document.querySelector("#search-value").value;
+function getWeatherData(city = "jerusalem") {
+    var cityName;
+    var cityTypedFromUser = document.querySelector("#search-value").value;
+
+    if (cityTypedFromUser.length === 0) {
+        cityName = city;
+    } else {
+        cityName = cityTypedFromUser;
+    }
+
+    var savedWeatherSearches =
+        JSON.parse(localStorage.getItem("savedWeather")) || [];
+    if (!savedWeatherSearches.includes(cityName)) {
+        savedWeatherSearches.push(cityName);
+        localStorage.setItem("savedWeather", JSON.stringify(savedWeatherSearches));
+        createList(savedWeatherSearches);
+    }
 
     fetch(
             "https://api.openweathermap.org/data/2.5/weather?q=" +
@@ -32,13 +47,21 @@ function getWeatherData() {
             );
             currentWeatherCard.appendChild(image);
 
-            var humidity = document.createElement("feels-like");
-            humidity.textContent = humidity.main;
+            var humidity = document.createElement("div");
+            humidity.textContent = "Humidity:" + data.main.humidity;
             currentWeatherCard.appendChild(humidity);
 
-            var temperature = document.createElement("sweating");
-            temperature.textContent = data.name;
+            var temperature = document.createElement("div");
+            temperature.textContent = "Temp: " + data.main.temp;
             currentWeatherCard.appendChild(temperature);
+
+            var wind = document.createElement("div");
+            wind.textContent = "WindSpeed:" + data.wind.speed;
+            currentWeatherCard.appendChild(wind);
+
+            var uv = document.createElement("div");
+            uv.setAttribute("class", "uv");
+            currentWeatherCard.appendChild(uv);
 
             //create element for store the humidity in
             //assign the testcontent opf the above element whatever the humidity
@@ -56,7 +79,7 @@ function getWeatherData() {
         });
 }
 
-//5 day 
+//5 day
 function getForecast(latitude, longitude) {
     fetch(
             `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&exclude=minutely,hourly,alerts&appid=${API_KEY}`
@@ -66,47 +89,78 @@ function getForecast(latitude, longitude) {
         })
         .then((data) => {
             console.log(data);
-
-
-
-            // data.daily.length
-            for (var i = 0; i <= 5; i++) {
+            document.getElementsByClassName("uv").textContent =
+                "UV:" + data.current.uvi;
+            document.querySelector(".forecastCards").innerHTML = ""
+                // data.daily.length
+            for (var i = 0; i < 5; i++) {
                 //console.log(i);
                 var forecast1 = document.createElement("div");
-                var forecast1humidity = document.createElement("p");
-                forecast1humidity.textContent = data.daily[i].humidity;
+                forecast1.setAttribute("class", "col-2");
+                var forecast1humidity = document.createElement("div");
+                forecast1humidity.textContent = "Humidity:" + data.daily[i].humidity;
+                var forecasttemp = document.createElement("div");
+                var celsiusTemp = temperatureConverter(data.daily[i].temp.day).slice(0, 4)
+                forecasttemp.textContent = "Temp:" + celsiusTemp;
+                var forecast3uv = document.createElement("div");
+                forecast3uv.textContent = "UV:" + data.daily[i].uvi;
+                var image = document.createElement("img");
+                image.setAttribute(
+                    "src",
+                    `http://openweathermap.org/img/wn/${data.daily[i].weather[0].icon}.png`
+                );
+                var date = new Date(data.daily[i].dt);
+                var dateDisplay = document.createElement("div");
+                dateDisplay.textContent = data.daily[i].dt;
+
+                forecast1.appendChild(dateDisplay);
+                forecast1.appendChild(image);
+
                 forecast1.appendChild(forecast1humidity);
+                forecast1.appendChild(forecasttemp);
+                forecast1.appendChild(forecast3uv);
                 document.querySelector(".forecastCards").appendChild(forecast1);
-
-
-
-                var forecast2 = document.createElement("div");
-                var forecast2temp = document.createElement("p");
-                forecast2temp.textContent = data.daily[i].temp.day;
-                forecast2.appendChild(forecast2temp);
-                document.querySelector(".forecastCards").appendChild(forecast2);
-
-
-                var forecast3 = document.createElement("div");
-                var forecast3uv = document.createElement("p");
-                forecast3uv.textContent = data.daily[i].uvi;
-                forecast3.appendChild(forecast3uv);
-                document.querySelector(".forecastCards").appendChild(forecast3);
             }
-
 
             //Going to be creating 5 cards - for data.daily[0] up to data.daily[4]
             //these will be being appended to the forecast div
-
         });
 }
 
 function temperatureConverter(valNum) {
-    valNum = parseFloat(valNum);
-    document.getElementById("outputCelcius").innerHTML = valNum - 273.15;
+    var celsius = parseInt(valNum) - 273.15;
+    return celsius.toString();
 }
 
-//  getWeatherData('Jerusalem')
+function createList(listOfCities) {
+    console.log("Create called")
+    var list = document.getElementById("list");
+    console.log(list)
+    list.innerHTML = "";
+    for (var i = 0; i < listOfCities.length; i++) {
+        var listItem = document.createElement("button");
+        listItem.textContent = listOfCities[i];
+        listItem.addEventListener("click", getWeatherData(listItem[i]))
+
+        list.appendChild(listItem);
+
+    }
+}
+
+function clearWeather() {
+    localStorage.clear();
+    window.location.reload();
+}
+
 document
     .querySelector(".search-button")
     .addEventListener("click", getWeatherData);
+
+var savedWeatherSearches =
+    JSON.parse(localStorage.getItem("savedWeather")) || [];
+if (savedWeatherSearches.length > 0) {
+    createList(savedWeatherSearches);
+    getWeatherData(savedWeatherSearches[savedWeatherSearches.length - 1]);
+} else {
+    getWeatherData();
+}
